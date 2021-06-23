fad595d4b02dbe1de5a3d4f91550d4bbc585a0d3
jeyasinghalex <jsinghalex0022@gmail.com>
2021-06-24 02:31:39 +0530

    minimum moves to equal array elements

---
+++ LeetCodeJun_2021/MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,17 @@
+public class MinimumMovesToEqualArrayElements {
+
+    public int minMoves(int[] nums) {
+
+        int min = Integer.MAX_VALUE;
+        for (int n : nums) {
+            min = Math.min(min, n);
+        }
+
+        int ans = 0;
+        for (int n : nums) {
+            ans += n - min;
+        }
+
+        return ans;
+    }
+}
