071d96197a3fd162a0b7a608fbd142577038fdad
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-12 20:48:35 +0530

    minimum cost to move chips to the same positio

---
+++ LeetCodeJuly_2021/MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,16 @@
+public class MinimumCostToMoveChipsToTheSamePosition {
+
+    public int minCostToMoveChips(int[] position) {
+
+        int odd = 0, even = 0;
+        for (int n : position) {
+
+            if ((n & 1) == 0) {
+                ++even;
+            } else {
+                ++odd;
+            }
+        }
+        return Math.min(even, odd);
+    }
+}
