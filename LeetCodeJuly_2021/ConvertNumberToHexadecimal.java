394b206728336ae979a88b68bc48020286c55687
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-14 19:51:47 +0530

    convert number to hexadecimal

---
+++ LeetCodeJuly_2021/ConvertNumberToHexadecimal.java
@@ -0,0 +1,17 @@
+public class ConvertNumberToHexadecimal {
+
+    public String toHex(int num) {
+
+        if (num == 0) {
+            return "0";
+        }
+        char[] hexchars = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        StringBuilder sb = new StringBuilder();
+
+        while (num != 0) {
+            sb.append(hexchars[(num & 15)]);
+            num >>>= 4;
+        }
+        return sb.reverse().toString();
+    }
+}
