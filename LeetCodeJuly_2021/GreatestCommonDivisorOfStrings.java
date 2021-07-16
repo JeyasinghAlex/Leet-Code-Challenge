5af945da3ea79bf260f53f7ade52f9b8022862c0
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-17 00:57:13 +0530

    greatest common divisor of string

---
+++ LeetCodeJuly_2021/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,15 @@
+public class GreatestCommonDivisorOfStrings {
+
+    public String gcdOfStrings(String str1, String str2) {
+
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcdVal = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdVal);
+    }
+
+    private int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+}
