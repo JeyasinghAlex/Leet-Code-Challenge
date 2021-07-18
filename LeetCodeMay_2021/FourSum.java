6cb8a81de6fb980fab22aa278a0c6870be05ef98
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-19 00:54:50 +0530

    four sum

--- LeetCodeMay_2021/FourSum.java
+++ LeetCodeMay_2021/FourSum.java
@@ -1,21 +1,22 @@
-package LeetCodeMay_2021;
-
 import java.util.*;
 
 public class FourSum {
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
-        Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
+        Set<List<Integer>> ans = new HashSet<>();
+        mergeSort(nums);
         for (int i = 0; i < nums.length; ++i) {
+
             for (int j = i + 1; j < nums.length; ++j) {
+
                 int lo = j + 1;
                 int hi = nums.length - 1;
                 while (lo < hi) {
+
                     int sum = nums[i] + nums[j] + nums[lo] + nums[hi];
                     if (sum == target) {
-                        set.add(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]));
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]));
                         ++lo;
                         --hi;
                     } else if (sum < target) {
@@ -26,6 +27,58 @@ public class FourSum {
                 }
             }
         }
-        return new ArrayList(set);
+        return new ArrayList(ans);
+    }
+
+    private void sort(int[] nums) {
+
+        for (int i = 1; i < nums.length; ++i) {
+            int hole = i;
+            int val = nums[hole];
+            while (hole > 0 && nums[hole - 1] > val) {
+                nums[hole] = nums[hole - 1];
+                hole--;
+            }
+            nums[hole] = val;
+        }
+    }
+
+    private void mergeSort(int[] arr) {
+
+        int n = arr.length;
+        if (n <= 1) return ;
+        int mid = n >>> 1;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+
+        for (int i = 0; i < mid; ++i)
+            left[i] = arr[i];
+        for (int i = mid; i < n; ++i)
+            right[i - mid] = arr[i];
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    private void merge(int[] arr, int[] left, int[] right) {
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.length && j < right.length) {
+
+            if (left[i] <= right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
     }
 }
