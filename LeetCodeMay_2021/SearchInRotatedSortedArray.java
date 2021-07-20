c2d698fd241f62b2828183d329d7af4faac89d6c
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-20 09:39:52 +0530

    search in rotated sorted array

--- LeetCodeMay_2021/SearchInRotatedSortedArray.java
+++ LeetCodeMay_2021/SearchInRotatedSortedArray.java
@@ -1,8 +1,34 @@
-package LeetCodeMay_2021;
 
 public class SearchInRotatedSortedArray {
 
-    public static int search(int[] nums, int target) {
+    public int search(int[] nums, int target) {
+
+        int st = 0;
+        int ed = nums.length;
+        while (st < ed) {
+            int mid = st + ed >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > nums[st]) {
+                if (target >= nums[st] && target <= nums[mid]) {
+                    ed = mid;
+                } else {
+                    st = mid + 1;
+                }
+            }
+            else {
+                if (target >= nums[mid] && target <= nums[ed - 1]) {
+                    st = mid + 1;
+                } else {
+                    ed = mid;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int search_1(int[] nums, int target) {
         int lo = 0;
         int hi = nums.length - 1;
         while (lo <= hi) {
