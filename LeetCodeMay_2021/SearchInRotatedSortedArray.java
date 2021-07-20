521333fdda6f2cc32d3dcf0007f134af350f5bdd
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-21 02:04:30 +0530

    search in rotated sorted array

--- LeetCodeMay_2021/SearchInRotatedSortedArray.java
+++ LeetCodeMay_2021/SearchInRotatedSortedArray.java
@@ -3,6 +3,52 @@ public class SearchInRotatedSortedArray {
 
     public int search(int[] nums, int target) {
 
+        int minIdx = minIndex(nums);
+        if (nums[minIdx] == target) {
+            return minIdx;
+        }
+
+        int lo = 0;
+        int hi = nums.length;
+        if (target >= nums[minIdx] && target <= nums[hi - 1]) {
+            lo = minIdx;
+        } else {
+            hi = minIdx;
+        }
+
+        while (lo < hi) {
+            int mid = lo + hi >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target > nums[mid]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return -1;
+
+    }
+
+    private int minIndex(int[] nums) {
+
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo < hi) {
+
+            int mid = lo + hi >>> 1;
+            if (nums[mid] > nums[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+
+    public int search_1(int[] nums, int target) {
+
         int st = 0;
         int ed = nums.length;
         while (st < ed) {
@@ -28,7 +74,7 @@ public class SearchInRotatedSortedArray {
         return -1;
     }
 
-    public static int search_1(int[] nums, int target) {
+    public static int search_2(int[] nums, int target) {
         int lo = 0;
         int hi = nums.length - 1;
         while (lo <= hi) {
