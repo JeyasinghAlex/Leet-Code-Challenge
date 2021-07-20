388a9ef94999c61691589716ca275f3f259d3a53
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-20 09:40:20 +0530

    find first and last position of element in sorted array

--- LeetCodeApr-2021/FindFirstAndLastPositionOfElementInSortedArray.java
+++ LeetCodeApr-2021/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -2,6 +2,50 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
 
     public int[] searchRange(int[] nums, int target) {
 
+        int a = lowerBond(nums, target);
+        int b = upperBond(nums, target);
+        if (nums.length == 0 || a >= nums.length) {
+            return new int[]{-1, -1};
+        }
+        if (nums[a] == target) {
+            return new int[]{a, b};
+        }
+        return new int[]{-1, -1};
+    }
+
+    private int lowerBond(int[] arr, int target) {
+
+        int lo = 0;
+        int hi = arr.length;
+
+        while (lo < hi) {
+            int mid = lo + hi >>> 1;
+            if (target > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+
+    private int upperBond(int[] arr, int target) {
+
+        int lo = 0;
+        int hi = arr.length;
+        while (lo < hi) {
+            int mid = lo + hi >>> 1;
+            if (target < arr[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo - 1;
+    }
+
+    public int[] searchRange_1(int[] nums, int target) {
+
         int l = 0, r = nums.length - 1;
         int st = 0,  ed = 0;
         boolean isFind = false;
