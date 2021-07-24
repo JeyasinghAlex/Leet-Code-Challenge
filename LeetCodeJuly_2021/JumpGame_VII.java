518264dd3df2d64644bdb7f5bec13ea1c17e7b12
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-24 22:22:42 +0530

    jump game 7

--- LeetCodeJuly_2021/JumpGame_VII.java
+++ LeetCodeJuly_2021/JumpGame_VII.java
@@ -7,6 +7,38 @@ public class JumpGame_VII {
         if(s.charAt(s.length() - 1) != '0')
             return false;
 
+        Queue<Integer> qu = new LinkedList<>();
+        qu.offer(0);
+        for (int i = 0; i < s.length(); ++i) {
+
+
+            if (s.charAt(i) == '0') {
+
+                while (!qu.isEmpty() && qu.peek() + maxJump < i) {
+                    qu.poll();
+                }
+
+                if (qu.isEmpty()) {
+                    return false;
+                }
+
+                int prev = qu.peek();
+                if (prev + maxJump >= i && prev + minJump <= i) {
+                    qu.offer(i);
+
+                    if (i == s.length() - 1) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean canReach_1(String s, int minJump, int maxJump) {
+        if(s.charAt(s.length() - 1) != '0')
+            return false;
+
         int maxReach = 0;
         Queue<Integer> queue = new LinkedList<>();
         queue.add(0);
