0cf62bd39e7fc4c0cfe341d66510798ac12b5e61
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-25 19:17:52 +0530

    rotate matrix

--- LeetCodeApr-2021/RotateImage.java
+++ LeetCodeApr-2021/RotateImage.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class RotateImage {
 
     public void rotate(int[][] matrix) {
@@ -12,6 +10,26 @@ public class RotateImage {
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
                 matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+
+    public void rotate_1(int[][] matrix) {
+
+        int n = matrix.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n/2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
             }
         }
     }
