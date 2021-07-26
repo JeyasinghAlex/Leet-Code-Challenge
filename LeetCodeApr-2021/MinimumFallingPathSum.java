d4b1865d1eb630c7ff1f3c4cfbc3f635271372ad
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-26 20:07:41 +0530

    minimum falling path sum

--- LeetCodeApr-2021/MinimumFallingPathSum.java
+++ LeetCodeApr-2021/MinimumFallingPathSum.java
@@ -2,13 +2,12 @@ public class MinimumFallingPathSum {
 
     public int minFallingPathSum(int[][] matrix) {
 
-        int row = matrix.length;
-        int col = matrix[0].length;
-        for (int i = 1; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
+        int n = matrix.length;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (j == 0) {
                     matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i - 1][j + 1]);
-                } else if(j == col - 1) {
+                } else if(j == n - 1) {
                     matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i - 1][j - 1]);
                 } else {
                     int ans = Math.min(matrix[i - 1][j - 1], matrix[i - 1][j]);
@@ -18,7 +17,7 @@ public class MinimumFallingPathSum {
         }
 
         int min = Integer.MAX_VALUE;
-        for (int a : matrix[row - 1]) {
+        for (int a : matrix[n - 1]) {
             min = Math.min(min, a);
         }
 
