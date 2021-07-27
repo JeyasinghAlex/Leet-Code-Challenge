b635a78f89012e4a8b4819bf86608e821aea2306
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-28 02:25:00 +0530

    max area of island

--- LeetCodeJun_2021/MaxAreaOfIsland.java
+++ LeetCodeJun_2021/MaxAreaOfIsland.java
@@ -1,8 +1,38 @@
-package LeetCodeJun_2021;
-
 public class MaxAreaOfIsland {
 
     public int maxAreaOfIsland(int[][] grid) {
+
+        int row = grid.length;
+        int col = grid[0].length;
+        int max = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j, 0);
+                    max = Math.max(max, area);
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int r, int c, int area) {
+
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0) {
+            return area;
+        }
+
+        ++area;
+        grid[r][c] = 0;
+        area = dfs(grid, r, c + 1, area);
+        area = dfs(grid, r, c - 1, area);
+        area = dfs(grid, r + 1, c, area);
+        area = dfs(grid, r - 1, c, area);
+        return area;
+    }
+
+    public int maxAreaOfIsland_1(int[][] grid) {
 
         int max = 0;
         boolean[][] visited = new boolean[grid.length][grid[0].length];
