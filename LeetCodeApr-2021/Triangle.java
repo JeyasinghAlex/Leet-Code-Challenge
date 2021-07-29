4cbeccfe7686d505cc2c5ddc431d12d565a66d0e
jeyasinghalex <jsinghalex0022@gmail.com>
2021-07-29 20:21:48 +0530

    triangle

--- LeetCodeApr-2021/Triangle.java
+++ LeetCodeApr-2021/Triangle.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Triangle {
 
+    public static void main(String[] args) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(Arrays.asList(4, 1, 8, 3));
+        ans.add(Arrays.asList(6,5,7));
+        ans.add(Arrays.asList(3,4));
+        ans.add(Arrays.asList(2));
+        System.out.println(new Triangle().minimumTot(ans));
+    }
+
     public int minimumTot(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[] dp = new int[n];
+        int[] dp = new int[n + 1];
         for (int i = 0; i < triangle.get(n - 1).size(); ++i) {
             dp[i] = triangle.get(n - 1).get(i);
         }
@@ -16,6 +26,23 @@ public class Triangle {
             }
         }
         return dp[0];
+    }
+
+    public int minimumTotal_1(List<List<Integer>> triangle) {
+
+        int n = triangle.size() - 1;
+        List<Integer> prev = triangle.get(n);
+
+        for (int i = n - 1; i >= 0; --i) {
+
+            List<Integer> curr = triangle.get(i);
+            for (int j = 0; j < curr.size(); ++j) {
+                int max = curr.get(j) + Math.min(prev.get(j), prev.get(j + 1));
+                curr.set(j, max);
+            }
+            prev = curr;
+        }
+        return triangle.get(0).get(0);
     }
 
     public int minimumTotal(List<List<Integer>> triangle) {
