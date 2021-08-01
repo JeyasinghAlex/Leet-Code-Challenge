5fbddb71a4b6ec5fffff4c64b314c79ef0b5945a
jeyasinghalex <jsinghalex0022@gmail.com>
2021-08-01 20:15:53 +0530

    k th largets element in an array

---
+++ LeetCodeJuly_2021/KthLargestElementInAnArray.java
@@ -0,0 +1,65 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthLargestElementInAnArray {
+
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+
+            int start = 0;
+            int end = nums.length - 1;
+            int index = nums.length - k;
+            while (start < end) {
+                int point = partition(nums, start, end);
+                if (index > point) {
+                    start = point + 1;
+                } else if (index < point) {
+                    end = point - 1;
+                } else {
+                    return nums[point];
+                }
+            }
+            return nums[start];
+        }
+
+        private int partition(int[] nums, int lo, int  hi) {
+
+            int pivot = nums[lo];
+            int start = lo;
+            int end = hi;
+            while (start < end) {
+
+                while (start <= hi && nums[start] <= pivot) {
+                    ++start;
+                }
+                while (end >= lo && nums[end] > pivot) {
+                    --end;
+                }
+
+                if (start < end) {
+                    swap(nums, start, end);
+                }
+            }
+            swap(nums, lo, end);
+            return end;
+        }
+
+        private void swap(int[] nums, int start, int end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+        }
+    }
+
+    public int findKthLargest_1(int[] nums, int k) {
+
+        Queue<Integer> qu = new PriorityQueue<>();
+        for (int n : nums) {
+            qu.offer(n);
+            if (qu.size() > k) {
+                qu.poll();
+            }
+        }
+        return qu.peek();
+    }
+}
