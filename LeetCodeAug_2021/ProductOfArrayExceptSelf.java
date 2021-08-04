e1eafd10488f3996f1a1c5d24168e6164986dc2c
jeyasinghalex <jsinghalex0022@gmail.com>
2021-08-04 21:06:46 +0530

    product of array except self

--- LeetCodeAug_2021/ProductOfArrayExceptSelf.java
+++ LeetCodeAug_2021/ProductOfArrayExceptSelf.java
@@ -2,6 +2,23 @@ public class ProductOfArrayExceptSelf {
 
     public int[] productExceptSelf(int[] nums) {
 
+        int[] ans = new int[nums.length];
+        int product = 1;
+        for (int i = 0; i < nums.length; ++i) {
+            product *= nums[i];
+            ans[i] = product;
+        }
+
+        product = 1;
+        for (int i = nums.length - 1; i > 0; --i) {
+            ans[i] = product * ans[i - 1];
+            product *= nums[i];
+        }
+        ans[0] = product;
+        return ans;
+    }
+
+    public int[] productExceptSelf_1(int[] nums) {
 
         int[] product = new int[nums.length];
         product[0] = nums[0];
@@ -23,7 +40,7 @@ public class ProductOfArrayExceptSelf {
         return ans;
     }
 
-    public int[] productExceptSelf_1(int[] nums) {
+    public int[] productExceptSelf_2(int[] nums) {
 
         int[] left = new int[nums.length];
         int[] right = new int[nums.length];
