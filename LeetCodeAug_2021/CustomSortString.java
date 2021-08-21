a66549d8aa88ac7414928c302e03f9710feb6e30
jeyasinghalex <jsinghalex0022@gmail.com>
2021-08-21 23:57:18 +0530

    custom sort string

---
+++ LeetCodeAug_2021/CustomSortString.java
@@ -0,0 +1,24 @@
+public class CustomSortString {
+
+    public String customSortString(String order, String s) {
+
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : order.toCharArray()) {
+            while (freq[ch - 'a']-- > 0) {
+                sb.append(ch);
+            }
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            while (freq[i]-- > 0) {
+                sb.append((char) (i + 'a'));
+            }
+        }
+        return sb.toString();
+    }
+}
