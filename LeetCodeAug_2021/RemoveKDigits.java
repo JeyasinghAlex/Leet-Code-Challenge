68102aba344ea8d0d73012dd9cbaf64724d85869
jeyasinghalex <jsinghalex0022@gmail.com>
2021-08-26 22:57:26 +0530

    remove digits

---
+++ LeetCodeAug_2021/RemoveKDigits.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class RemoveKDigits {
+
+    public String removeKdigits(String num, int k) {
+
+        if (k == num.length()) {
+            return "0";
+        }
+
+        Stack<Character> st = new Stack<>();
+        for (char ch : num.toCharArray()) {
+
+            while (k > 0 && !st.isEmpty() && st.peek() > ch) {
+                st.pop();
+                --k;
+            }
+            st.push(ch);
+        }
+
+        while (k-- > 0) {
+            st.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        sb.reverse();
+
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+}
