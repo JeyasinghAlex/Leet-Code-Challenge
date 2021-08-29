68e398536f95add8c6ff8ab1b6da4dfe6bf24295
jeyasinghalex <jsinghalex0022@gmail.com>
2021-08-29 20:45:10 +0530

    sum of two integers

---
+++ LeetCodeAug_2021/SumOfTwoIntegers.java
@@ -0,0 +1,12 @@
+public class SumOfTwoIntegers {
+
+    public int getSum(int a, int b) {
+
+        int result = a ^ b;
+        int carry = (a & b) << 1;
+        if (carry != 0) {
+            return getSum(result, carry);
+        }
+        return result;
+    }
+}
