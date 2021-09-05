4a79bf81bf0b01071146b3aa9e2afc14f0aeb9cc
jeyasinghalex <jsinghalex0022@gmail.com>
2021-09-05 21:39:44 +0530

    valid triangle number

---
+++ LeetCodeSep_2021/ValidTriangleNumber.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ValidTriangleNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,4};
+        System.out.println(new ValidTriangleNumber().triangleNumber(arr));
+    }
+
+    public int triangleNumber(int[] nums) {
+
+        if (nums.length < 3) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            int lo = 0;
+            int hi = i - 1;
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] > nums[i]) {
+                    count += (hi - lo);
+                    --hi;
+                } else {
+                    ++lo;
+                }
+            }
+        }
+        return count;
+    }
+}
