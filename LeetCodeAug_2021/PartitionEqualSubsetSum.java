355d80933a9ade2da81cc9573dade8c92c7fb3e9
jeyasinghalex <jsinghalex0022@gmail.com>
2021-09-17 21:05:33 +0530

    0/1 knapsack

--- LeetCodeAug_2021/PartitionEqualSubsetSum.java
+++ LeetCodeAug_2021/PartitionEqualSubsetSum.java
@@ -2,6 +2,37 @@ public class PartitionEqualSubsetSum {
 
     public boolean canPartition(int[] nums) {
 
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+
+        int target = sum >>> 1;
+        boolean[][] dp = new boolean[n + 1][target + 1];
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= target; ++j) {
+
+                if (j < nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+
+    public boolean canPartition_1(int[] nums) {
+
         int sum = 0;
         for (int i = 0; i < nums.length; ++i) {
             sum += nums[i];
--- LeetCodeSep_2021/KnapsackProblem_0_1.java
+++ LeetCodeSep_2021/KnapsackProblem_0_1.java
@@ -3,12 +3,21 @@ import java.util.Arrays;
 public class KnapsackProblem_0_1 {
 
     public static void main(String[] args) {
-        int[] w = {2, 3, 4, 5};
-        int[] p = {1, 2, 5, 6};
-        System.out.println(new KnapsackProblem_0_1().knapsack(w, p, 8));
+
+        int[] profit = {1, 2, 5, 6};
+        int[] weight = {2, 3, 4, 5};
+        int[][] memo = new int[4 + 1][8 + 1];
+        for (int i = 0; i < memo.length; ++i){
+            for (int j = 0; j < memo[0].length; ++j) {
+                memo[i][j] = -1;
+            }
+        }
+
+        System.out.println(new KnapsackProblem_0_1().recursive(weight, profit, profit.length    , 8));
+//        System.out.println(Arrays.deepToString(memo));
     }
 
-    public int knapsack(int[] weight, int[] profit, int w) {
+    public int dp(int[] weight, int[] profit, int w) {
 
         int n = weight.length;
 
@@ -23,5 +32,37 @@ public class KnapsackProblem_0_1 {
             }
         }
         return dp[n][w];
+    }
+
+    public int memoization(int[] weight, int[] profit, int[][] memo, int n, int w) {
+
+        if (w == 0 || n == 0) {
+            return 0;
+        }
+        if (memo[n][w] != -1) {
+            return memo[n][w];
+        }
+
+        if (weight[n - 1] > w) {
+            return memoization(weight, profit, memo, n - 1, w);
+        } else {
+            int findNext = memoization(weight, profit, memo, n - 1, w);
+            int include = profit[n - 1] + memoization(weight, profit, memo, n - 1, w - weight[n - 1]);
+            memo[n][w] = Math.max(findNext, include);
+        }
+        return memo[n][w];
+    }
+
+    public int recursive(int[] weight, int[] profit, int n, int w) {
+
+        if (w == 0 || n == 0) {
+            return 0;
+        } else if (weight[n - 1] > w) {
+            return recursive(weight, profit, n - 1, w);
+        } else {
+            int findNext = recursive(weight, profit, n - 1, w);
+            int include = profit[n - 1] + recursive(weight, profit, n - 1, w - weight[n - 1]);
+            return Math.max(findNext, include);
+        }
     }
 }
