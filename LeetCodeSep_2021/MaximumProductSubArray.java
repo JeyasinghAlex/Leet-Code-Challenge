43de545d3fd7e0757d43e2878e3465038d30039c
jeyasinghalex <jsinghalex0022@gmail.com>
2021-09-19 22:46:12 +0530

    maximum product subarray

---
+++ LeetCodeSep_2021/MaximumProductSubArray.java
@@ -0,0 +1,28 @@
+public class MaximumProductSubArray {
+
+    public int maxProduct(int[] nums) {
+
+        int max = Integer.MIN_VALUE;
+        int mul = 1;
+
+        for (int i = 0; i < nums.length; ++i) {
+
+            mul *= nums[i];
+            max = Math.max(max, mul);
+            if (mul == 0) {
+                mul = 1;
+            }
+        }
+
+        mul = 1;
+        for (int i = nums.length - 1; i >= 0; --i) {
+
+            mul *= nums[i];
+            max = Math.max(max, mul);
+            if (mul == 0) {
+                mul = 1;
+            }
+        }
+        return max;
+    }
+}
