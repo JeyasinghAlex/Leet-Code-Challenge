96a3046458c4a0d68b557b990e58047a50fb0e4d
jeyasinghalex <jsinghalex0022@gmail.com>
2021-09-24 21:13:03 +0530

    palindrome partitioning

--- LeetCodeAug_2021/PalindromePartitioning.java
+++ LeetCodeAug_2021/PalindromePartitioning.java
@@ -5,6 +5,53 @@ public class PalindromePartitioning {
 
     public List<List<String>> partition(String s) {
 
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            if (s.charAt(i - 1) == s.charAt(i)) {
+                dp[i - 1][i] = true;
+            }
+        }
+        for (int i = 2; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+
+                int k = i + j;
+                if (s.charAt(j) == s.charAt(k) && dp[j + 1][k - 1]) {
+                    dp[j][k] = true;
+                }
+            }
+        }
+        List<List<String>> ans = new ArrayList<>();
+        backtrack(0, ans, new ArrayList<>(), dp, s, "");
+        return ans;
+    }
+
+
+    private void backtrack(int start, List<List<String>> ans, List<String> list, boolean[][] dp, String s, String sub) {
+
+        if (start == s.length()) {
+            ans.add(new ArrayList<>(list));
+            return ;
+        }
+
+        for (int i = start; i < s.length(); ++i) {
+
+            if (dp[start][i]) {
+                String substring = s.substring(start, i + 1);
+                list.add(substring);
+                backtrack(i + 1, ans, list, dp, s, s.substring(i + 1));
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public List<List<String>> partition_1(String s) {
+
         List<List<String>> ans = new ArrayList<>();
         List<String> list = new ArrayList<>();
         dfs(ans, list, 0, s);
