2e1a6a159bb6ddb2d54fe961bb281c8e4e485cb1
jeyasinghalex <jsinghalex0022@gmail.com>
2021-09-27 22:02:19 +0530

    longest palindromic subsequence

--- LeetCodeAug_2021/LongestPalindromicSubsequence.java
+++ LeetCodeAug_2021/LongestPalindromicSubsequence.java
@@ -4,27 +4,21 @@ public class LongestPalindromicSubsequence {
 
         int n = s.length();
         int[][] dp = new int[n][n];
-
-        int max = 1;
         for (int i = 0; i < n; ++i) {
             dp[i][i] = 1;
         }
 
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 0; j < n - i + 1; ++j) {
-
-                int k = i + j - 1;
-                if (i == 2 && s.charAt(j) == s.charAt(k)) {
-                    dp[j][k] = 2;
-                } else if (s.charAt(j) ==  s.charAt(k)) {
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                int k = i + j;
+                if (s.charAt(j) == s.charAt(k)) {
                     dp[j][k] = 2 + dp[j + 1][k - 1];
                 } else {
                     dp[j][k] = Math.max(dp[j][k - 1], dp[j + 1][k]);
                 }
-                max = Math.max(max, dp[j][k]);
             }
         }
-        return max;
+        return dp[0][n - 1];
     }
 
     private int memoization(String s, int l, int r, int[][] memo) {
