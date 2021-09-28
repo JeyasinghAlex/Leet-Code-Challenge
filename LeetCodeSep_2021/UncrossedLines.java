a202866942c6190af432af49605d346370954f04
jeyasinghalex <jsinghalex0022@gmail.com>
2021-09-28 22:25:33 +0530

    uncrossed lines

---
+++ LeetCodeSep_2021/UncrossedLines.java
@@ -0,0 +1,21 @@
+public class UncrossedLines {
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[][] lcs = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    lcs[i][j] = 1 + lcs[i - 1][j - 1];
+                } else {
+                    lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+                }
+            }
+        }
+        return lcs[m][n];
+    }
+}
