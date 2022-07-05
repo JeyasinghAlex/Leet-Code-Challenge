f42335c09eeefa2613227641bfca942f838c4a0b
Alex Sparrow <jsinghalex0022@gmail.com>
2022-07-05 22:12:16 +0530

    candy

---
+++ LeetCodeJuly_2022/Candy.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Candy {
+
+    public int candy(int[] ratings) {
+
+        int n = ratings.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += Math.max(left[i], right[i]);
+        }
+        return ans;
+    }
+
+    public int candy_1(int[] ratings) {
+
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        int ans = candies[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+            ans += candies[i];
+        }
+        return ans;
+    }
+}
