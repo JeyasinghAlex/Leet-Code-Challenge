d2a457c86d7c0658724658371fa0fe033c721084
Alex Sparrow <jsinghalex0022@gmail.com>
2022-07-09 10:36:08 +0530

    find eventual safe states

--- LeetCodeOct_2021/FindEventualSafeStates.java
+++ LeetCodeOct_2021/FindEventualSafeStates.java
@@ -11,20 +11,26 @@ public class FindEventualSafeStates {
         int[] color = new int[nodeCount];
 
         for(int i = 0;i < nodeCount;i++){
-            if(dfs(graph, i, color))    res.add(i);
+            boolean flag = dfs(graph, i, color);
+            if(flag)   {
+                res.add(i);
+            }
         }
-
         return res;
     }
     public boolean dfs(int[][] graph, int start, int[] color){
-        if(color[start] != 0)   return color[start] == 1;
+        if(color[start] != 0)   {
+            return color[start] == 1;
+        }
 
         color[start] = 2;
         for(int newNode : graph[start]){
-            if(!dfs(graph, newNode, color))    return false;
+            boolean flag = dfs(graph, newNode, color);
+            if(!flag) {
+                return false;
+            }
         }
         color[start] = 1;
-
         return true;
     }
 }
