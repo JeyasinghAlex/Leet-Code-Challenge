17b97116690630504ab1d45e1efa5bca33b79ce9
Alex Sparrow <jsinghalex0022@gmail.com>
2022-07-14 00:04:33 +0530

    Cheapest flights with in k stops

--- LeetCodeOct_2021/CheapestFlightsWithinKStops.java
+++ LeetCodeOct_2021/CheapestFlightsWithinKStops.java
@@ -27,4 +27,44 @@ public class CheapestFlightsWithinKStops {
         return path[dst] != Integer.MAX_VALUE ? path[dst] :  -1;
     }
 
+    public int findCheapestPrice_1(int n, int[][] flights, int src, int dst, int k) {
+
+        List<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; ++i) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : flights) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            adj[u].add(new int[]{v, w});
+        }
+
+        Queue<int[]> qu = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        qu.offer(new int[]{src, 0, k + 1});
+
+        while (!qu.isEmpty()) {
+
+            int[] arr = qu.poll();
+            int curr = arr[0];
+            int weight = arr[1];
+            int stop = arr[2];
+
+            if (dst == curr) {
+                return weight;
+            }
+
+            if (stop > 0) {
+                for (int[] edge : adj[curr]) {
+                    int v = edge[0];
+                    int w = edge[1];
+
+                    qu.offer(new int[]{v, weight + w, stop - 1});
+                }
+            }
+        }
+        return -1;
+    }
 }
