65e65e5d12d3ded8f20deea267252854865cd785
Alex Sparrow <jsinghalex0022@gmail.com>
2022-07-22 22:16:26 +0530

    partition list

--- LeetCodeNov_2021/PartitionList.java
+++ LeetCodeNov_2021/PartitionList.java
@@ -22,4 +22,53 @@ public class PartitionList {
         firstHead.next = second.next;
         return first.next;
     }
+
+    public ListNode partition_1(ListNode head, int x) {
+
+        int n = getLength(head);
+        if (n <= 1) {
+            return head;
+        }
+
+        int[] nums = new int[n];
+        ListNode node = head;
+
+        int idx = 0;
+        while (node != null) {
+            nums[idx++] = node.val;
+            node = node.next;
+        }
+
+        for (int i = 0; i < n; ++i) {
+
+            if (nums[i] < x) {
+
+                int prev = i - 1;
+                int val = nums[i];
+                while (prev >= 0 && val < nums[prev] && nums[prev] >= x) {
+                    nums[prev + 1] = nums[prev];
+                    prev--;
+                }
+
+                nums[prev + 1] = val;
+            }
+        }
+
+        node = head;
+        for (int i = 0; i < n; ++i) {
+            node.val = nums[i];
+            node = node.next;
+        }
+        return head;
+    }
+
+    private int getLength(ListNode node) {
+
+        int count = 0;
+        while (node != null) {
+            ++count;
+            node = node.next;
+        }
+        return count;
+    }
 }
