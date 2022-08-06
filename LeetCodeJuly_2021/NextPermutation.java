4f43e377d89b74c5ab94f67d5b9b42106c0070af
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-06 22:16:30 +0530

    next permutation

--- LeetCodeJuly_2021/NextPermutation.java
+++ LeetCodeJuly_2021/NextPermutation.java
@@ -1,6 +1,37 @@
 public class NextPermutation {
 
     public void nextPermutation(int[] nums) {
+
+        int n = nums.length;
+        if (n <= 1) {
+            return;
+        }
+
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            --i;
+        }
+
+        int j = n - 1;
+        if (i >= 0) {
+            while (nums[j] <= nums[i]) {
+                --j;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, n - 1);
+    }
+
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
+            ++i;
+            --j;
+        }
+    }
+
+    public void nextPermutation_1(int[] nums) {
         int pivot = indexOfLastPeek(nums) - 1;
         if (pivot != -1) {
             int nextPrefix = lastIndexOfGreater(nums, nums[pivot]);
