4dafd140bef0cbf9689a79725aa8b888c7d29df6
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-12 23:32:58 +0530

    lowest common ancestor of binary serarch tree

--- LeetCodeJuly_2021/LowestCommonAncestorOfBinarySearchTree.java
+++ LeetCodeJuly_2021/LowestCommonAncestorOfBinarySearchTree.java
@@ -2,10 +2,34 @@ public class LowestCommonAncestorOfBinarySearchTree {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null) {
+            return right;
+        }
+        else if (right == null) {
+            return left;
+        }
+        else {
+            return root;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor_1(TreeNode root, TreeNode p, TreeNode q) {
+
         if (root.val > Math.max(p.val, q.val)) {
-            return lowestCommonAncestor(root.left, p, q);
+            return lowestCommonAncestor_1(root.left, p, q);
         } else if (root.val < Math.min(p.val, q.val)) {
-            return lowestCommonAncestor(root.right, p, q);
+            return lowestCommonAncestor_1(root.right, p, q);
         } else {
             return root;
         }
