a44faf99528316b75dca80f86c2f5911f89120d2
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-14 22:56:34 +0530

    rotate list

--- LeetCodeNov_2021/RotateList.java
+++ LeetCodeNov_2021/RotateList.java
@@ -2,6 +2,67 @@ public class RotateList {
 
     public ListNode rotateRight(ListNode head, int k) {
 
+
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        ListNode node = head;
+        int len = 1;
+
+        while (node.next != null) {
+            ++len;
+            node = node.next;
+        }
+
+        node.next = head;
+
+        k = len - k % len;
+
+        while (k-- > 0) {
+            node = node.next;
+        }
+        head = node.next;
+        node.next = null;
+        return head;
+    }
+
+    public ListNode rotateRight_1(ListNode head, int k) {
+
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        int len = getLength(head);
+
+        k = k % len;
+
+        if (k == 0) {
+            return head;
+        }
+
+        for (int i = 1; i <= k; ++i) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        ListNode tail = fast;
+        tail.next = head;
+        head = slow.next;
+        slow.next = null;
+        return head;
+    }
+
+    public ListNode rotateRight_2(ListNode head, int k) {
+
         if (head == null) {
             return null;
         }
