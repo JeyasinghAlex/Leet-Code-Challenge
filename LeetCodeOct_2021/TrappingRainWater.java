d2a4c6e1c95462b4cc57178d4f9400823388b2b0
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-16 00:56:05 +0530

    trapping rain water

--- LeetCodeOct_2021/TrappingRainWater.java
+++ LeetCodeOct_2021/TrappingRainWater.java
@@ -3,6 +3,39 @@ public class TrappingRainWater {
     public int trap(int[] height) {
 
         int n = height.length;
+
+        int left = 0;
+        int right = n - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int ans = 0;
+
+        while (left <= right) {
+
+            if (height[left] <= height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    ans += leftMax - height[left];
+                }
+                ++left;
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    ans += rightMax - height[right];
+                }
+                --right;
+            }
+        }
+        return ans;
+    }
+
+    public int trap_1(int[] height) {
+
+        int n = height.length;
         if (n <= 2) {
             return 0;
         }
@@ -30,7 +63,7 @@ public class TrappingRainWater {
         return ans;
     }
 
-    public int trap_1(int[] height) {
+    public int trap_2(int[] height) {
 
         int n = height.length;
         if (n <= 2) {
