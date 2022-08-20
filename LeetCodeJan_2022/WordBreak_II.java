1039c5f7a63fefa43bf57d526e21c2aad3e238c8
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-20 22:29:35 +0530

    word break 2

--- LeetCodeJan_2022/WordBreak_II.java
+++ LeetCodeJan_2022/WordBreak_II.java
@@ -7,6 +7,32 @@ public class WordBreak_II {
 
     public List<String> wordBreak(String s, List<String> wordDict) {
 
+        List<String> ans = new ArrayList<>();
+        backtrack(s, "", wordDict, ans);
+        return ans;
+    }
+
+
+    private void backtrack(String s, String str, List<String> wordDict, List<String> ans) {
+
+        if (s.length() == 0) {
+            str = str.substring(0, str.length() - 1);
+            ans.add(str);
+            return ;
+        }
+
+        for (int i = 0; i <= s.length(); ++i) {
+
+            String substr = s.substring(0, i);
+
+            if (wordDict.contains(substr)) {
+                backtrack(s.substring(i), str + substr + ' ', wordDict, ans);
+            }
+        }
+    }
+
+    public List<String> wordBreak_1(String s, List<String> wordDict) {
+
         Set<String> set = new HashSet<>(wordDict);
         List<String> list = new ArrayList<>();
         wordBreak(s, "", set, list, s.length(), 0);
