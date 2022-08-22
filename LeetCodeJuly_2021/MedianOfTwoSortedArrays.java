f341319cab97cc65510c77042eadd341c66334d1
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-22 23:39:47 +0530

    median of two sorted arrays

--- LeetCodeJuly_2021/MedianOfTwoSortedArrays.java
+++ LeetCodeJuly_2021/MedianOfTwoSortedArrays.java
@@ -2,6 +2,47 @@ public class MedianOfTwoSortedArrays {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        if (n1 > n2) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int lo = 0;
+        int hi = n1;
+
+        while (lo <= hi) {
+
+            int cut1 = lo + hi >>> 1;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int l1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int r1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (l1 <= r2 && l2 <= r1) {
+
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+            }
+
+            if (l1 > r2) {
+                hi = cut1 - 1;
+            } else {
+                lo = cut1 + 1;
+            }
+        }
+        return 0.0;
+    }
+
+    public double findMedianSortedArrays_1(int[] nums1, int[] nums2) {
+
         int n = nums1.length;
         int m = nums2.length;
         int[] temp = new int[n + m];
