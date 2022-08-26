eca0d4c7102e50b250ef96bc931f0cfe44775a1c
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-26 22:59:21 +0530

    kth largest element in an array - using quick select

--- LeetCodeJuly_2021/KthLargestElementInAnArray.java
+++ LeetCodeJuly_2021/KthLargestElementInAnArray.java
@@ -3,53 +3,58 @@ import java.util.Queue;
 
 public class KthLargestElementInAnArray {
 
-    class Solution {
-        public int findKthLargest(int[] nums, int k) {
-
-            int start = 0;
-            int end = nums.length - 1;
-            int index = nums.length - k;
-            while (start < end) {
-                int point = partition(nums, start, end);
-                if (index > point) {
-                    start = point + 1;
-                } else if (index < point) {
-                    end = point - 1;
-                } else {
-                    return nums[point];
-                }
-            }
-            return nums[start];
-        }
-
-        private int partition(int[] nums, int lo, int  hi) {
-
-            int pivot = nums[lo];
-            int start = lo;
-            int end = hi;
-            while (start < end) {
-
-                while (start <= hi && nums[start] <= pivot) {
-                    ++start;
-                }
-                while (end >= lo && nums[end] > pivot) {
-                    --end;
-                }
-
-                if (start < end) {
-                    swap(nums, start, end);
-                }
-            }
-            swap(nums, lo, end);
-            return end;
-        }
-
-        private void swap(int[] nums, int start, int end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-        }
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 3, 1, 2, 4, 5, 5, 6};
+        int k = 5;
+        System.out.println(new KthLargestElementInAnArray().findKthLargest(arr, k));
     }
+
+    public int findKthLargest(int[] nums, int k) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        int index = nums.length - k;
+        while (start < end) {
+            int point = partition(nums, start, end);
+            if (index > point) {
+                start = point + 1;
+            } else if (index < point) {
+                end = point - 1;
+            } else {
+                return nums[point];
+            }
+        }
+        return nums[start];
+    }
+
+    private int partition(int[] nums, int lo, int hi) {
+
+        int pivot = nums[lo];
+        int start = lo;
+        int end = hi;
+        while (start < end) {
+
+            while (start <= hi && nums[start] <= pivot) {
+                ++start;
+            }
+            while (end >= lo && nums[end] > pivot) {
+                --end;
+            }
+
+            if (start < end) {
+                swap(nums, start, end);
+            }
+        }
+        swap(nums, lo, end);
+        return end;
+    }
+
+    private void swap(int[] nums, int start, int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+
 
     public int findKthLargest_1(int[] nums, int k) {
 
