19d65c070eaf1ec152660c64429acf7de13a9c81
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-31 00:36:13 +0530

    next smaller element

---
+++ LeetCodeAug_2022/NextSmallerElement.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class NextSmallerElement {
+
+    public int[] NextSmallerElement(int[] nums) {
+
+        int n = nums.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; ++i) {
+
+            while (!st.isEmpty() && nums[i] <= st.peek()) {
+                st.pop();
+            }
+
+            int val = st.isEmpty() ? -1 : st.peek();
+
+            ans[i] = val;
+
+            st.push(nums[i]);
+        }
+        return ans;
+    }
+}
