f05819c4d92c777f254f215dbc2bb89f18ce3392
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-31 00:35:51 +0530

    sort stack

---
+++ LeetCodeAug_2022/SortStack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class SortStack {
+
+    public void sortStack(Stack<Integer> st) {
+
+        if (st.isEmpty()) {
+            return ;
+        }
+        int val = st.pop();
+        sortStack(st);
+        sort(st, val);
+    }
+
+    private void sort(Stack<Integer> st, int val) {
+
+        Stack<Integer> temp = new Stack<>();
+
+        while (!st.isEmpty()) {
+
+            int top = st.peek();
+            if (val < top) {
+                temp.push(st.pop());
+            } else {
+                break;
+            }
+        }
+
+        st.push(val);
+
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+    }
+}
