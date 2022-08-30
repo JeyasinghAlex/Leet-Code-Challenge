92ca8ef63530f13b173e2460cc5c3b4c3df6fd0b
Alex Sparrow <jsinghalex0022@gmail.com>
2022-08-30 23:57:10 +0530

    next greater elememnt 1 using stack

--- LeetCodeJun_2021/NextGreaterElement_I.java
+++ LeetCodeJun_2021/NextGreaterElement_I.java
@@ -1,9 +1,39 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class NextGreaterElement_I {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; --i) {
+
+            while (!st.isEmpty() && nums2[i] >= st.peek()) {
+                st.pop();
+            }
+
+            int val = st.isEmpty() ? -1 : st.peek();
+
+            map.put(nums2[i], val);
+
+            st.push(nums2[i]);
+        }
+
+        int[] ans = new int[m];
+
+        for (int i = 0; i < m; ++i) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+
+    public int[] nextGreaterElement_1(int[] nums1, int[] nums2) {
 
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums2.length; ++i) {
