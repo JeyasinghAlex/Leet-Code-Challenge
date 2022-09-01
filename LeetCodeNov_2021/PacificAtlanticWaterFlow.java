6005c3bd876cdc3d0a7927b91d94e1c0efd73183
Alex Sparrow <jsinghalex0022@gmail.com>
2022-09-02 00:32:25 +0530

    pacific atlantic water flow

--- LeetCodeNov_2021/PacificAtlanticWaterFlow.java
+++ LeetCodeNov_2021/PacificAtlanticWaterFlow.java
@@ -5,9 +5,54 @@ import java.util.List;
 public class PacificAtlanticWaterFlow {
 
 
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+
+        int row = heights.length;
+        int col = heights[0].length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
+
+        for (int i = 0; i < row; ++i) {
+            dfs(i, 0, i, 0, heights, pacific);
+            dfs(i, col - 1, i, col - 1, heights, atlantic);
+        }
+
+        for (int i = 0; i < col; ++i) {
+            dfs(0, i, 0, i, heights, pacific);
+            dfs(row - 1, i, row - 1, i, heights, atlantic);
+        }
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.add(Arrays.asList(i, j));
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    private void dfs(int r, int c, int lr, int lc, int[][] heights, boolean[][] visited) {
+
+        if (r < 0 || r >= heights.length || c < 0 || c >= heights[0].length || visited[r][c] || heights[r][c] < heights[lr][lc]) {
+            return ;
+        }
+
+        visited[r][c] = true;
+
+        dfs(r + 1, c, r, c, heights, visited);
+        dfs(r - 1, c, r, c, heights, visited);
+        dfs(r, c + 1, r, c, heights, visited);
+        dfs(r, c - 1, r, c, heights, visited);
+
+    }
+
     int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    public List<List<Integer>> pacificAtlantic_1(int[][] heights) {
 
         int row = heights.length;
         int col = heights[0].length;
