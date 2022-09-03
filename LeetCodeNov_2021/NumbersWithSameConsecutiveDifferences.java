e0323a63b77d9a0e5902915941eff2fb6094211a
Alex Sparrow <jsinghalex0022@gmail.com>
2022-09-04 00:19:10 +0530

    number with same consecutive differences

--- LeetCodeNov_2021/NumbersWithSameConsecutiveDifferences.java
+++ LeetCodeNov_2021/NumbersWithSameConsecutiveDifferences.java
@@ -5,6 +5,38 @@ public class NumbersWithSameConsecutiveDifferences {
 
     public int[] numsSameConsecDiff(int n, int k) {
 
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= 9; ++i) {
+            backtrack(i, n - 1, k, i, list);
+        }
+
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); ++i) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+
+    private void backtrack(int prev, int n, int k, int num, List<Integer> list) {
+
+        if (n == 0) {
+            list.add(num);
+            return ;
+        }
+
+        for (int i = 0; i <= 9; ++i) {
+
+            if (Math.abs(prev - i) == k) {
+                backtrack(i, n - 1, k, (num * 10) + i, list);
+            }
+        }
+    }
+
+    public int[] numsSameConsecDiff_1(int n, int k) {
+
         if (n == 1) {
             return new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         }
