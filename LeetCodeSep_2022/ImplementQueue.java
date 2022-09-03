bc42f827ede588f21a374633e50f751827493cd2
Alex Sparrow <jsinghalex0022@gmail.com>
2022-09-03 12:47:31 +0530

    leet code sep 2022

---
+++ LeetCodeSep_2022/ImplementQueue.java
@@ -0,0 +1,55 @@
+public class ImplementQueue {
+
+    class Node {
+
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    Queue() {
+        // Implement the Constructor
+    }
+
+    private Node head;
+    private Node tail;
+    /*----------------- Public Functions of Queue -----------------*/
+
+    boolean isEmpty() {
+        // Implement the isEmpty() function
+        return head == null;
+    }
+
+    void enqueue(int data) {
+        // Implement the enqueue() function
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+        }
+        tail = node;
+    }
+
+    int dequeue() {
+        // Implement the dequeue() function
+        if (head == null) {
+            return -1;
+        }
+
+        int val = head.val;
+        head = head.next;
+        return val;
+    }
+
+    int front() {
+        // Implement the front() function
+        if (head == null) {
+            return -1;
+        }
+        return head.val;
+    }
+}
