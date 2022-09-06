e9a8a17f20d49c7e045c2bee27fe85eeeb8800cf
Alex Sparrow <jsinghalex0022@gmail.com>
2022-09-06 23:40:12 +0530

    robinkarp

---
+++ LeetCodeSep_2022/RabinCarp.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RabinCarp {
+
+
+    public static List<Integer> stringMatch_1(String str, String pat) {
+
+        int n = pat.length();
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i <= str.length() - n; ++i) {
+
+            String substr = str.substring(i, n + i);
+
+            if (pat.equals(substr)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
