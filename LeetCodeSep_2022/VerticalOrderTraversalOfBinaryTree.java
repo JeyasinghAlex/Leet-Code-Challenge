e7f0d9200211a1978d9472f70a3618760d70200d
Alex Sparrow <jsinghalex0022@gmail.com>
2022-09-17 01:04:03 +0530

    vertical order traversal of binary tree

---
+++ LeetCodeSep_2022/VerticalOrderTraversalOfBinaryTree.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class VerticalOrderTraversalOfBinaryTree {
+
+    class Node {
+
+        int line;
+        TreeNode treeNode;
+
+        public Node (TreeNode treeNode, int line) {
+            this.line = line;
+            this.treeNode = treeNode;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Map<Integer, List<Integer>> lineVsVal = new TreeMap<>();
+
+        Queue<Node> qu = new LinkedList<>();
+
+        qu.offer(new Node(root, 0));
+
+        while (!qu.isEmpty()) {
+
+            int len = qu.size();
+
+            Map<Integer, Queue<Integer>> sMap = new TreeMap<>();
+
+            while (len-- > 0) {
+
+                Node node = qu.poll();
+
+                int line = node.line;
+                TreeNode treeNode = node.treeNode;
+
+                if (treeNode.left != null) {
+                    qu.offer(new Node(treeNode.left, line - 1));
+                }
+
+                if (treeNode.right != null) {
+                    qu.offer(new Node(treeNode.right, line + 1));
+                }
+
+                sMap.putIfAbsent(line, new PriorityQueue<>());
+                sMap.get(line).offer(treeNode.val);
+            }
+
+            for (Map.Entry<Integer, Queue<Integer>> entry : sMap.entrySet()) {
+                int line = entry.getKey();
+                lineVsVal.putIfAbsent(line, new ArrayList<>());
+                Queue<Integer> pq = entry.getValue();
+                while (!pq.isEmpty()) {
+                    lineVsVal.get(line).add(pq.poll());
+                }
+            }
+        }
+
+        for (List<Integer> list : lineVsVal.values()) {
+            ans.add(list);
+        }
+        return ans;
+    }
+}
