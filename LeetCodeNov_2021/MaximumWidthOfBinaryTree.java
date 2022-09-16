d02d358b3d941d2391d3427c1248f5f87831bd38
Alex Sparrow <jsinghalex0022@gmail.com>
2022-09-16 11:05:54 +0530

    maximum width of binary tree

--- LeetCodeNov_2021/MaximumWidthOfBinaryTree.java
+++ LeetCodeNov_2021/MaximumWidthOfBinaryTree.java
@@ -1,8 +1,66 @@
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class MaximumWidthOfBinaryTree {
 
+    static class Pair {
+
+        int val;
+        TreeNode node;
+
+        public Pair(TreeNode node, int val) {
+            this.val = val;
+            this.node = node;
+        }
+    }
+
     public int widthOfBinaryTree(TreeNode root) {
+
+        if  (root == null) {
+            return 0;
+        }
+
+        Queue<Pair> qu = new LinkedList<>();
+        qu.offer(new Pair(root, 0));
+
+        int first = 0;
+        int last = 0;
+        int max = Integer.MIN_VALUE;
+
+        while (!qu.isEmpty()) {
+
+            int len = qu.size();
+            int min = qu.peek().val;
+            int size = len;
+
+            while (len-- > 0) {
+
+                Pair pair = qu.poll();
+                TreeNode node = pair.node;
+
+                int currId = pair.val - min;
+
+                if (len == size - 1) {
+                    first = currId;
+                }
+                if (len == 0) {
+                    last = currId;
+                }
+
+                if (node.left != null) {
+                    qu.offer(new Pair(node.left, 2 * currId + 1));
+                }
+                if (node.right != null) {
+                    qu.offer(new Pair(node.right, 2 * currId + 2));
+                }
+
+            }
+            max = Math.max(max, last - first + 1);
+        }
+        return max;
+    }
+
+    public int widthOfBinaryTree_1(TreeNode root) {
         if (root == null) {
             return 0;
         }
