c2789efcbd971cb35e879cd0614998a35dd39599
Alex Sparrow <jsinghalex0022@gmail.com>
2022-10-07 23:30:49 +0530

    my calender 3

---
+++ LeetCodeOct_2022/MyCalendar_III.java
@@ -0,0 +1,20 @@
+public class MyCalendar_III {
+
+    private TreeMap<Integer, Integer> treeMap;
+    public MyCalendarThree() {
+        this.treeMap = new TreeMap<>();
+    }
+
+    public int book(int start, int end) {
+        treeMap.put(start, treeMap.getOrDefault(start, 0) + 1);
+        treeMap.put(end, treeMap.getOrDefault(end, 0) - 1);
+
+        int onGoingEvent = 0;
+        int max = 0;
+        for (int event : treeMap.values()) {
+            onGoingEvent += event;
+            max = Math.max(max, onGoingEvent);
+        }
+        return max;
+    }
+}
