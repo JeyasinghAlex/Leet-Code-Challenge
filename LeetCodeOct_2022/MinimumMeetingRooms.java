93b89d5b674bf2a4ef1482d7b2b3e45a264ac037
Alex Sparrow <jsinghalex0022@gmail.com>
2022-10-14 23:25:22 +0530

    minimum meeting rooms

---
+++ LeetCodeOct_2022/MinimumMeetingRooms.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MinimumMeetingRooms {
+
+    public int solve(int[][] meetings) {
+
+        int n = meetings.length;
+        int ans = 0;
+
+        Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        Queue<Integer> qu = new PriorityQueue<>();
+
+        for (int i = 0; i < n; ++i) {
+
+            int[] meeting = meetings[i];
+            int start = meeting[0];
+            int end = meeting[1];
+
+            if (!qu.isEmpty() && start >= qu.peek()) {
+                qu.poll();
+            }
+            qu.offer(end);
+        }
+        return qu.size();
+    }
+}
