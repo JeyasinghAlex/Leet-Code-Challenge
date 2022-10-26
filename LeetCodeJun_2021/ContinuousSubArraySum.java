d8f413952dfba5743a039b22166cf5f5a8c7f19b
Alex Sparrow <jsinghalex0022@gmail.com>
2022-10-27 00:58:56 +0530

    continuous subarray sum

--- LeetCodeJun_2021/ContinuousSubArraySum.java
+++ LeetCodeJun_2021/ContinuousSubArraySum.java
@@ -1,4 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContinuousSubArraySum {
 
+    public boolean checkSubarraySum(int[] nums, int k) {
 
+        int n = nums.length;
+        Set<Integer> set = new HashSet<>();
+
+        int prevSum = 0;
+        int currSum = 0;
+
+        for (int i = 0; i < n; ++i) {
+            currSum += nums[i];
+            currSum %= k;
+            if (set.contains(currSum)) {
+                return true;
+            }
+            set.add(prevSum);
+            prevSum = currSum;
+        }
+        return false;
+    }
 }
