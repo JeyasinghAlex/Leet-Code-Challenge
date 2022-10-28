a2ce1582bfee4cc4438fe80b4993590ece79f66d
Alex Sparrow <jsinghalex0022@gmail.com>
2022-10-28 21:44:14 +0530

    group anagrams

--- LeetCodeAug_2021/GroupAnagrams.java
+++ LeetCodeAug_2021/GroupAnagrams.java
@@ -5,7 +5,36 @@ import java.util.Map;
 
 public class GroupAnagrams {
 
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagramsUsingPrimeNumbers(String[] strs) {
+
+        List<List<String>> ans = new ArrayList<>();
+        Map<Long, List<String>> map = new HashMap<>();
+        int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+        for (String s : strs) {
+            Long key = getKey(s, primes);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        for (List<String> list : map.values()) {
+            ans.add(list);
+        }
+        return ans;
+    }
+
+
+    private long getKey(String s, int[] primes) {
+
+        int mod = 1_000_000_007;
+        long key = 1L;
+
+        for (char ch : s.toCharArray()) {
+            key = key * primes[ch - 'a'] % mod;
+        }
+        return key;
+    }
+
+    public List<List<String>> groupAnagramsUsingCount(String[] strs) {
 
         Map<String, List<String>> map = new HashMap<>();
 
