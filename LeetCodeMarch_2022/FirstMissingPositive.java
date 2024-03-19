48baee9ee74ef904e3b1366967a6de06400eb044
Alex Sparrow <jsinghalex0022@gmail.com>
2024-03-19 21:22:48 +0530

    first mission positive

--- LeetCodeMarch_2022/FirstMissingPositive.java
+++ LeetCodeMarch_2022/FirstMissingPositive.java
@@ -3,6 +3,32 @@ public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
 
         int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+
+            int index = Math.abs(nums[i]) - 1;
+
+            if (index < n && nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if(nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    public int firstMissingPositive_1(int[] nums) {
+
+        int n = nums.length;
         int idx = 0;
         while (idx < n) {
             if (nums[idx] > 0 && nums[idx] <= n && nums[idx] != nums[nums[idx] - 1]) {
