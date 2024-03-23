b417fc8913c9aafe5dfd1ca380d728fced7a35f2
Alex Sparrow <jsinghalex0022@gmail.com>
2024-03-23 23:32:28 +0530

    Daily temperature

--- LeetCodeNov_2021/DailyTemperatures.java
+++ LeetCodeNov_2021/DailyTemperatures.java
@@ -5,6 +5,24 @@ public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
 
         int n = temperatures.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+
+        for (int i = n - 1; i >= 0; --i) {
+
+            while (!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]) {
+                st.pop();
+            }
+
+            int val = st.isEmpty() ? 0 : st.peek() - i;
+            st.push(i);
+            ans[i] = val;
+        }
+        return ans;
+    }
+    public int[] dailyTemperatures_1(int[] temperatures) {
+
+        int n = temperatures.length;
         int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
 
