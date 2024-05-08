6f669ae0de5cbe3509f4d3711c7f07910b33235e
Alex Sparrow <51689568+JeyasinghAlex@users.noreply.github.com>
2024-05-08 22:20:15 +0530

    Update InterleavingString.java

--- LeetCodeOct_2021/InterleavingString.java
+++ LeetCodeOct_2021/InterleavingString.java
@@ -1,6 +1,41 @@
 public class InterleavingString {
 
     public boolean isInterleave(String s1, String s2, String s3) {
+        
+        int l = s1.length();
+        int m = s2.length();
+        int n = s3.length();
+
+        if (l + m != n) {
+            return false;
+        }
+
+        Boolean[][] dp = new Boolean[l + 1][m + 1];
+        return helper(0, 0, s1, s2, s3, dp);
+    }
+
+    private boolean helper(int idx1, int idx2, String s1, String s2, String s3, Boolean[][] dp) {
+
+        if (idx1 == s1.length() && idx2 == s2.length()) {
+            return true;
+        }
+
+        if (dp[idx1][idx2] != null) {
+            return dp[idx1][idx2];
+        }
+        
+        boolean isValid = false;
+        if (idx1 < s1.length() && s1.charAt(idx1) == s3.charAt(idx1 + idx2)) {
+            isValid |= helper(idx1 + 1, idx2, s1, s2, s3, dp);
+        }
+        if (!isValid && idx2 < s2.length() && s2.charAt(idx2) == s3.charAt(idx1 + idx2)) {
+            isValid |= helper(idx1, idx2 + 1, s1, s2, s3, dp);
+        }
+        dp[idx1][idx2] = isValid;
+        return dp[idx1][idx2];
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3) {
 
         int m = s1.length();
         int n =  s2.length();
