932c4153c093c4e91a46b3c3d46491ee53aa9ef9
Alex Sparrow <jsinghalex0022@gmail.com>
2024-07-21 00:47:39 +0530

    first missing number

--- InterviewBit/FirstMissingInteger.java
+++ InterviewBit/FirstMissingInteger.java
@@ -1,4 +1,21 @@
 package InterviewBit;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FirstMissingInteger {
+
+    public int firstMissingPositive(int[] A) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int n : A)
+            set.add(n);
+
+        for (int i = 1; i <= Integer.MAX_VALUE; ++i) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
